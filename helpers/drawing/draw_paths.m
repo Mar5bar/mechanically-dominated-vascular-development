ee4f7e4f12865b168d0706d108c31dd458d75660
Benjamin Walker <benjaminwalker@blueyonder.co.uk>
2022-01-27 16:51:44 +0000

    fast suffix removed from draw_path_fast.m. Output directory now contains blank .gitignore

--- helpers/drawing/draw_paths.m
+++ helpers/drawing/draw_paths.m
@@ -2,6 +2,6 @@ function [trails, collisions, endpoints] = draw_paths(labels,starts,ends,trails)
     collisions = logical(zeros(size(starts,1),1));
     endpoints = zeros(size(starts,1),2);
     for i = 1 : size(starts,1)
-        [trails, collisions(i), endpoints(i,:)] = draw_path_fast(labels(i),starts(i,:),ends(i,:),trails);
+        [trails, collisions(i), endpoints(i,:)] = draw_path(labels(i),starts(i,:),ends(i,:),trails);
     end
 end
